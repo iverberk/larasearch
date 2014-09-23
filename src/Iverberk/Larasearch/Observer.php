05abb7955fd051f1513c1819de2f84ed8b8c3435
Ferry de Groot <degroot@ict.eur.nl>
2014-09-23 15:55:54 +0200

    Fix for non existent relations

--- src/Iverberk/Larasearch/Observer.php
+++ src/Iverberk/Larasearch/Observer.php
@@ -27,29 +27,30 @@ class Observer {
 
 				// Define a little recursive function to walk the relations of the model based on the path
 				// Eventually it will queue all affected searchable models for reindexing
-				$walk = function ($relation) use (&$walk, &$path, &$reindexModels)
-				{
-					$segment = array_shift($path);
+                $walk = function ($relation) use (&$walk, &$path, &$reindexModels)
+                {
+                    $segment = array_shift($path);
 
-					if ($relation instanceof Model)
-					{
-                        $reindexModels[] = get_class($relation) . ':' . $relation->getKey();
-					}
-					else if ($relation instanceof Collection)
-					{
-						foreach ($relation as $record)
-						{
-							if (!empty($segment))
-							{
-								$walk($record->getRelation($segment));
-							}
-							else
-							{
-                                $reindexModels[] = get_class($relation) . ':' . $relation->getKey();
-							}
-						}
-					}
-				};
+                    $relation = $relation instanceof Collection ? $relation : new Collection([$relation]);
+                    foreach ($relation as $record)
+                    {
+                        if (!empty($segment))
+                        {
+                            if (in_array($segment, $record->getRelations()))
+                            {
+                                $walk($record->getRelation($segment));
+                            }
+                            else
+                            {
+                                return;
+                            }
+                        }
+                        else
+                        {
+                            $reindexModels[] = get_class($record) . ':' . $record->getKey();
+                        }
+                    }
+                };
 
 				$walk($model->getRelation(array_shift($path)));
 			}

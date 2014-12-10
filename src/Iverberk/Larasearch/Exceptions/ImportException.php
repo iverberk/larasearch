ff7ad27677c003ee504cb2dc962f14d807ba0a4a
Ivo Verberk <verberk@ict.eur.nl>
2014-12-10 21:10:49 +0100

    Dump the error items to the console

--- src/Iverberk/Larasearch/Exceptions/ImportException.php
+++ src/Iverberk/Larasearch/Exceptions/ImportException.php
@@ -7,7 +7,8 @@ class ImportException extends Exception {
 	// Redefine the exception so message isn't optional
 	public function __construct($message, $code = 0, $errorItems = [])
 	{
-		// TODO: Handle the error items in a graceful way
+		// Just dump the errorItems for now
+		var_dump($errorItems);
 
 		// make sure everything is assigned properly
 		parent::__construct($message, $code);

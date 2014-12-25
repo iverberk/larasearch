1c5d14082b29dc05e6ab748cf3d6a0d87b348ec0
Ivo Verberk <verberk@ict.eur.nl>
2014-12-25 16:53:57 +0100

    Remove the plain var_dump, needs a better logging solution

--- src/Iverberk/Larasearch/Exceptions/ImportException.php
+++ src/Iverberk/Larasearch/Exceptions/ImportException.php
@@ -7,9 +7,6 @@ class ImportException extends Exception {
 	// Redefine the exception so message isn't optional
 	public function __construct($message, $code = 0, $errorItems = [])
 	{
-		// Just dump the errorItems for now
-		var_dump($errorItems);
-
 		// make sure everything is assigned properly
 		parent::__construct($message, $code);
 	}
